24a6d6e379cc28ce10cee083f4828e61847459ff
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-12 00:28:21 +0400

    feat: add style to routers

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,38 +1,38 @@
 import { Link } from "react-router-dom"
 import ROUTERS from "../../constants/router_constants"
-
+import '../../style/layout/_navigation.scss';
 
 
 const Nav = () => {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className="navbar">
+            <ul className="navbar_ul">
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.HOME} className="nav_link">
                         Home
                     </Link>
                 </li>
-                <li>
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.MEN} className="nav_link">
                         Men
                     </Link>
                 </li>
-                <li>
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.WOMEN} className="nav_link">
                         Women
                     </Link>
                 </li>
-                <li>
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.SHOP} className="nav_link">
                         Shop
                     </Link>
                 </li>
-                <li>
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.CATEGORY} className="nav_link">
                         Category
                     </Link>
                 </li>
-                <li>
+                <li className="navbar_ul_li">
                     <Link to={ROUTERS.SALES} className="nav_link">
                         Sales
                     </Link>
