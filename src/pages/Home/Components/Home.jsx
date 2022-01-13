846c1884f01d59b14980025e0fdb4784080aa8d5
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-13 19:22:39 +0400

    feat: add homepage banner

--- src/pages/Home/Components/Home.jsx
+++ src/pages/Home/Components/Home.jsx
@@ -1,10 +1,30 @@
 import '../../../style/pages/_home.scss';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div className="home_page">
-            This is Home page;
-        </div>
+        <main className="home_page_content">
+           <Banner />
+           <div className='row'>
+               <div className='col-sm-12'>
+                   <div className='brand_area_image'>
+                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/02.png' alt='Brand'></img>
+                   </div>
+                   <div className='brand_area_image'>
+                        <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/03.png' alt='Brand'></img>
+                   </div>
+                   <div className='brand_area_image'>
+                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/04.png' alt='Brand'></img>
+                   </div>
+                   <div className='brand_area_image'>
+                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/05.png' alt='Brand'></img>
+                   </div>
+                   <div className='brand_area_image'>
+                       <img src="https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/01.png" alt="Brand" />
+                   </div>
+               </div>
+           </div>
+        </main>
     )
 }
 
