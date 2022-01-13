9ff1610d3fe239120d066d393e524f5a9ea60b97
Torniuke Machitidze <tornike.machitidze7@gmail.com>
2022-01-13 15:51:23 +0400

    Footer is added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,27 +10,31 @@ import Category from "../pages/Category/Components/Category";
 import Sales from "../pages/Sales/Components/Sales";
 import HeaderTop from "./header/Header_Top";
 import AccountPage from "../pages/Account/Components/AccountPage";
+import Footer from "../components/footer/Footer";
 
 const history = createBrowserHistory();
 
 const App = () => {
   return (
-    <div className="container">
-      <header>
-        <HeaderTop />
-        <Router history={history}>
-          <Nav />
-          <Routes>
-            <Route path={ROUTERS.HOME} exact element={<Home />} />
-            <Route path={ROUTERS.MEN} exact element={<Men />} />
-            <Route path={ROUTERS.WOMEN} exact element={<Women />} />
-            <Route path={ROUTERS.SHOP} exact element={<Shop />} />
-            <Route path={ROUTERS.CATEGORY} exact element={<Category />} />
-            <Route path={ROUTERS.ACCOUNT} exact element={<AccountPage />} />
-          </Routes>
-        </Router>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <header>
+          <HeaderTop />
+          <Router history={history}>
+            <Nav />
+            <Routes>
+              <Route path={ROUTERS.HOME} exact element={<Home />} />
+              <Route path={ROUTERS.MEN} exact element={<Men />} />
+              <Route path={ROUTERS.WOMEN} exact element={<Women />} />
+              <Route path={ROUTERS.SHOP} exact element={<Shop />} />
+              <Route path={ROUTERS.CATEGORY} exact element={<Category />} />
+              <Route path={ROUTERS.ACCOUNT} exact element={<AccountPage />} />
+            </Routes>
+          </Router>
+        </header>
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/footer/AccountFooter.jsx
@@ -0,0 +1,15 @@
+const AccountFooter = () => {
+  return (
+    <div className="account_container">
+      <h3>Account</h3>
+      <ul>
+        <li>Your Bag</li>
+        <li>Profile</li>
+        <li>Order Completed</li>
+        <li>Log-out</li>
+      </ul>
+    </div>
+  );
+};
+
+export default AccountFooter;
