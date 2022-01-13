ae8c58b91e8f39f4e945343e6dbf96720899874c
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-13 21:10:02 +0400

    feat: add homepage brands row

--- src/constants/url_constants.js
+++ src/constants/url_constants.js
@@ -1,5 +1,9 @@
 const url = {
-
+    brand1: 'https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/02.png',
+    brand2: 'https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/03.png',
+    brand3: 'https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/04.png',
+    brand4: 'https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/05.png',
+    brand5: 'https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/01.png'
 }
 
 export default url;
--- src/pages/Home/Components/Home.jsx
+++ src/pages/Home/Components/Home.jsx
@@ -1,29 +1,12 @@
 import '../../../style/pages/_home.scss';
 import Banner from './Banner';
+import Brands from './Brands';
 
 const Home = () => {
     return (
         <main className="home_page_content">
            <Banner />
-           <div className='row'>
-               <div className='col-sm-12'>
-                   <div className='brand_area_image'>
-                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/02.png' alt='Brand'></img>
-                   </div>
-                   <div className='brand_area_image'>
-                        <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/03.png' alt='Brand'></img>
-                   </div>
-                   <div className='brand_area_image'>
-                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/04.png' alt='Brand'></img>
-                   </div>
-                   <div className='brand_area_image'>
-                       <img src='https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/05.png' alt='Brand'></img>
-                   </div>
-                   <div className='brand_area_image'>
-                       <img src="https://demo.echotemplate.com/olog-ecommerce-responsive-html-template/dist/images/brand/01.png" alt="Brand" />
-                   </div>
-               </div>
-           </div>
+           <Brands />
         </main>
     )
 }
