da47927c580d324a60827d69e24bc7c6d2cf9156
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-14 16:37:55 +0400

    feat: add About-area layout,about component

---
+++ src/pages/Home/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='about_area'>
+            <div className='main_container'>
+                <div className='about_area_content'>
+                    <div className='align_items_center'>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
--- src/pages/Home/Components/Home.jsx
+++ src/pages/Home/Components/Home.jsx
@@ -1,21 +1,19 @@
-import Banner from './Banner';
-import Brands from './Brands';
-import Features from './Features';
-import '../../../style/pages/_home.scss';
-import '../../../style/layout/_grid.scss';
-
+import Banner from "./Banner";
+import Brands from "./Brands";
+import Features from "./Features";
+import About from "./About";
+import "../../../style/pages/_home.scss";
+import "../../../style/layout/_grid.scss";
 
 const Home = () => {
-    return (
-        <div className='main_container'>
-            <main className="home_page_content">
-           <Banner />
-           <Brands />
-           <Features />
-            </main>
-        </div>
-        
-    )
-}
+  return (
+    <main className="home_page_content">
+      <Banner />
+      <Brands />
+      <Features />
+      <About />
+    </main>
+  );
+};
 
 export default Home;
