0d3e0fc885c1628140edf7ad6b9c8ea3cb152749
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-17 23:33:26 +0400

    feat: apply prettier header, navbar

--- src/components/header/Header_Top.jsx
+++ src/components/header/Header_Top.jsx
@@ -6,12 +6,20 @@ import "../../style/layout/_header.scss";
 const HeaderTop = () => {
   return (
     <div className="header_top">
-      <div className="contact">
-        <Contact />
-      </div>
-      <div className="language_currency">
-        <Language />|
-        <Currency />
+      <div className="main_container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="header_top_wrapper">
+              <div className="contact">
+                <Contact />
+              </div>
+              <div className="language_currency">
+                <Language />|
+                <Currency />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
