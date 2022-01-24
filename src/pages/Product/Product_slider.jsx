b2c350a272b57d881fc2a98e9834f9373b61cbec
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-24 21:13:45 +0400

    feat: add Slick-Slider to product, add product components

---
+++ src/pages/Product/Product_slider.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
+import { getProduct } from "../../redux/actions";
+import Slider from "react-slick";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+import "../../style/components/_product.scss";
+import "../../style/layout/_grid.scss";
+import "../../style/pages/_home.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProductSlider = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const [nav1, setNav1] = useState();
+  const [nav2, setNav2] = useState();
+
+  useEffect(() => {
+    dispatch(getProduct(params.id));
+  }, [dispatch, params.id]);
+
+  const product = useSelector((store) => {
+    return store.product.product;
+  });
+
+  return (
+    <div className="product_slider">
+      <Slider asNavFor={nav2} ref={(slider1) => setNav1(slider1)}>
+        <div className="slider_img">
+          <TransformWrapper initialScale={1}>
+            {({ zoomIn, zoomOut, resetTransform }) => (
+              <>
+                <TransformComponent>
+                  <img
+                    src={product.image}
+                    alt="product"
+                    className="product_image"
+                  />
+                </TransformComponent>
+                <div className="tools">
+                  <button onClick={() => zoomIn()} className="btn_round">
+                    Zoom In
+                  </button>
+                  <button onClick={() => zoomOut()} className="btn_round">
+                    Zoom Out
+                  </button>
+                  <button
+                    onClick={() => resetTransform()}
+                    className="btn_round"
+                  >
+                    Reset
+                  </button>
+                </div>
+              </>
+            )}
+          </TransformWrapper>
+        </div>
+      </Slider>
+      <div className="slider_nav">
+        <Slider
+          asNavFor={nav1}
+          ref={(slider2) => setNav2(slider2)}
+          slidesToShow={3}
+          swipeToSlide={true}
+          focusOnSelect={true}
+          arrows={false}
+        >
+          <span>
+            <img src={product.image} alt="product" />
+          </span>
+          <span>
+            <img src={product.image} alt="product" />
+          </span>
+          <span>
+            <img src={product.image} alt="product" />
+          </span>
+          <span>
+            <img src={product.image} alt="" />
+          </span>
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSlider;
