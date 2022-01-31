7d1ce534fc370b7d7e0e8d377efe8b2ad687c1bf
Tamarchika <Ratiani.tamuna@gmail.com>
2022-01-31 18:27:46 +0400

    feat: add functionality to Search input

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "../../style/components/_input.scss";
 
 const SearchInput = () => {
+  const [searchId, setSearchId] = useState(null);
+  const searchProduct = (e) => {
+    setSearchId(e.target.value);
+  };
+
   return (
     <div className="search_input">
       <form>
         <input
           type="search"
-          value="Search for product..."
           className="input_placeholder"
+          placeholder="Search for the product with Id"
+          value={searchId}
+          onChange={searchProduct}
         />
         <span className="search_icon">
-          {<FontAwesomeIcon icon={faSearch} />}
+          <Link to={`/category/product/${searchId}`} tabIndex="-1">
+            {<FontAwesomeIcon icon={faSearch} />}
+          </Link>
         </span>
       </form>
     </div>
