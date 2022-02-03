4291e3f68112820180ab660c0f8e759410b67876
Torniuke Machitidze <tornike.machitidze7@gmail.com>
2022-02-03 16:37:08 +0400

    add loader to cart

--- src/pages/Account/Components/UserPage/Cart.jsx
+++ src/pages/Account/Components/UserPage/Cart.jsx
@@ -1,14 +1,19 @@
 import { useSelector } from "react-redux";
+import Loader from "react-spinners/ClipLoader";
 
 import CartItem from "./CartItem";
 import CartItemsHeader from "./CartItemsHeader";
 
 const Cart = () => {
   const cartState = useSelector((store) => store.userCart.cart);
-
   return (
     <div className="cart_items">
       <CartItemsHeader />
+      {cartState.length === 0 && (
+        <div style={{ margin: "100px auto", height: "400px" }}>
+          <Loader />
+        </div>
+      )}
       {cartState.map((prod) => {
         return <CartItem key={prod.product.id} data={prod} />;
       })}
