87a1875ee312864320c962fe5c682e82eaa4635a
Torniuke Machitidze <tornike.machitidze7@gmail.com>
2022-02-03 19:55:42 +0400

    delete spiner for cart

--- src/pages/Account/Components/UserPage/Cart.jsx
+++ src/pages/Account/Components/UserPage/Cart.jsx
@@ -9,11 +9,6 @@ const Cart = () => {
   return (
     <div className="cart_items">
       <CartItemsHeader />
-      {cartState.length === 0 && (
-        <div style={{ margin: "100px auto", height: "400px" }}>
-          <Loader />
-        </div>
-      )}
       {cartState.map((prod) => {
         return <CartItem key={prod.product.id} data={prod} />;
       })}
